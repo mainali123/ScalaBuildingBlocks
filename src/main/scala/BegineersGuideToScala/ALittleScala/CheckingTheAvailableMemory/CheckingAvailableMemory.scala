5b638b36c172d5061d73b53df87cad5a37c01759
Diwash Mainali <diwashmainali900@gmail.com>
2023-07-24 10:04:21 +0545

    Checking Available Memory

---
+++ src/main/scala/BegineersGuideToScala/ALittleScala/CheckingTheAvailableMemory/CheckingAvailableMemory.scala
@@ -0,0 +1,19 @@
+/*
+package BegineersGuideToScala.ALittleScala.CheckingTheAvailableMemory
+
+object CheckingAvailableMemory {
+
+  def main(args: Array[String]): Unit = {
+    val runtime = sys.runtime
+    val freeMemory = runtime.freeMemory()
+    val totalMemory = runtime.totalMemory()
+
+    println(s"Total memory is $totalMemory and free memory is $freeMemory")
+    println("Requesting system gc")
+    sys.runtime.gc()
+
+    val newFreeMemory = runtime.freeMemory()
+    println(s"Total memory is $totalMemory and free memory is now $newFreeMemory")
+  }
+}
+*/
---
+++ src/main/scala/BegineersGuideToScala/ScalaBuildingBlocks/TheBasicsOfTheLanguage/HelloWorld.scala
@@ -0,0 +1,11 @@
+package BegineersGuideToScala.ScalaBuildingBlocks.TheBasicsOfTheLanguage
+
+object HelloWorld extends App {
+  val name = "Diwash Mainali"
+  if (name.endsWith("Mainali")) {
+    println(s"Hello $name")
+  } else {
+    println("Hello, World!")
+  }
+
+}
